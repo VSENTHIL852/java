911fd6c61b3d0b94a1a90fe05136974e20d842c8
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2024-08-21 21:01:00 +0530

    Add files via upload

---
+++ aa.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class string
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the strings:");
+		String a=sc.nextLine();
+		String n,ch;
+		for (int i=0; i<a.length(); i++)
+     		 {
+	        ch= str.charAt(i);
+	        a= ch+n; 
+	       }
+      System.out.println("Reversed word: "+ );
+
+
+
+	}
+}
