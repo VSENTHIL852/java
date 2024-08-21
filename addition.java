fa18c18849d9d4f9073788700c3ea63b12b2ed59
SENTHILKUMAR V <120891309+VSENTHIL852@users.noreply.github.com>
2024-08-21 21:02:40 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,21 @@
+public void addition()
+{
+	int a=10,b=20;
+	System.out.println("Addition of two numbers:");
+	System.out.print(a+b);
+}
+
+
+
+public class function
+{
+	public static void main(String[] args)
+	{
+	  Methods o=new Methods();
+	  o.addition();
+
+
+
+	}
+}
+
